8571e55eb55b9f24ba2b6fe01343503970374df8
Will Furnass <will@thearete.co.uk>
2016-11-18 18:31:21 +0000

    x2go example server using Vagrant and Puppet: first commit

---
+++ manifests/default.pp
@@ -0,0 +1,37 @@
+package { "epel-release":
+  ensure => present,
+}
+
+package { "openssh-server":
+  ensure => present,
+}
+service { "sshd":
+  ensure  => "running",
+  require => Package["openssh-server"],
+}
+
+$x2go_pkgs = [ 'x2goserver', 'x2goserver-xsession' ]
+package { $x2go_pkgs:
+  ensure => present,
+}
+
+$xfce_pkgs = [ 'xfce4-settings', 'xfdesktop', 'xfce4-panel', 'xfce4-terminal', 'xfce4-session', 'xfwm4' ]
+package { $xfce_pkgs:
+  ensure => present,
+}
+
+group { 'users':
+    ensure => 'present',
+    gid    => '1000',
+}
+
+user { 'someuser':
+  ensure           => 'present',
+  gid              => '1500',
+  home             => '/home/someuser',
+  password         => '!!',
+  password_max_age => '99999',
+  password_min_age => '0',
+  shell            => '/bin/bash',
+  uid              => '1000',
+}
